dfe35e8cfac87a1399fdd319ec7428035c53b525
farhan <mfarhanhyd5@gmail.com>
2021-10-28 14:02:39 +0530

    minor fix

---
+++ lib/global_constant.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+class GlobalConstant {
+  static final String PATTERN =
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+}
+
+class GlobalResource {
+  static Color BLUE_COLOUR = Color(0xFF1167b1);
+  static Color DARK_BLUE = Color(0xFF001084);
+}
