894f05e67951e2025abf891ce31055d4901b50b1
Farhan <mfarhanhyd5@gmail.com>
2022-01-24 11:58:53 +0530

    form connected to database

--- lib/home/found_form.dart
+++ lib/home/found_form.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:lost_and_found/database/db_funtions.dart';
+import 'package:intl/intl.dart';
+import 'package:date_format/date_format.dart';
+import 'package:lost_and_found/home/home_page.dart';
+
+User? _userid;
 
 class FoundForm extends StatelessWidget {
-  const FoundForm({Key? key}) : super(key: key);
+  FoundForm(User? result) {
+    _userid = result;
+  }
 
   static const String _title = 'Found form';
 
@@ -29,7 +39,7 @@ class MyStatefulWidget extends StatefulWidget {
 
 /// This is the private State class that goes with MyStatefulWidget.
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  String dropdownValue = "government ids & certificate";
+  String _dropdownValue = "government ids & certificate";
   List<String> _itemset = [
     "Electronic items",
     "government ids & certificate",
@@ -38,6 +48,12 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
     "Book",
     "pet"
   ];
+
+  TextEditingController _brandController = TextEditingController();
+  TextEditingController _colorController = TextEditingController();
+  TextEditingController _descriptionController = TextEditingController();
+  TextEditingController _dateController = TextEditingController();
+  TextEditingController _timeController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +65,6 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
               children: <Widget>[
                 Padding(
                   padding: const EdgeInsets.all(15),
-
                   // dropdown
                   child: Container(
                     padding: EdgeInsets.all(15),
@@ -57,12 +72,12 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                         borderRadius: BorderRadius.circular(10),
                         border: Border.all(color: Colors.grey)),
                     child: DropdownButton<String>(
-                      value: dropdownValue,
+                      value: _dropdownValue,
                       icon: const Icon(Icons.arrow_drop_down),
                       iconSize: 24,
                       onChanged: (String? newValue) {
                         setState(() {
-                          dropdownValue = newValue!;
+                          _dropdownValue = newValue!;
                         });
                       },
                       items: _itemset
@@ -76,15 +91,16 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                   ),
                 ),
 
-                // brand name input field
+                // name input field
                 Padding(
                   padding: EdgeInsets.all(15),
                   child: TextField(
+                    controller: _brandController,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(15)),
-                      labelText: 'Brand name',
-                      hintText: 'Enter Brand',
+                      labelText: 'name',
+                      hintText: 'Enter name',
                     ),
                   ),
                 ),
@@ -93,6 +109,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                 Padding(
                   padding: EdgeInsets.all(15),
                   child: TextField(
+                    controller: _colorController,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(15)),
@@ -106,6 +123,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                 Padding(
                   padding: EdgeInsets.all(15),
                   child: TextField(
+                    controller: _descriptionController,
                     minLines: 2,
                     maxLines: 5,
                     keyboardType: TextInputType.multiline,
@@ -118,10 +136,10 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                   ),
                 ),
 
-                // Date
                 Padding(
                     padding: EdgeInsets.all(15),
                     child: TextField(
+                      controller: _dateController,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(15)),
@@ -129,25 +147,52 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                         labelText: 'Date',
                         hintText: "Date",
                       ),
-                      onTap: () {
-                        showDatePicker(
+                      onTap: () async {
+                        DateTime selectedDate = DateTime.now();
+                        final DateTime? picked = await showDatePicker(
                             context: context,
-                            initialDate: DateTime.now(),
-                            firstDate: DateTime(2000),
-                            lastDate: DateTime(2022));
+                            initialDate: selectedDate,
+                            initialDatePickerMode: DatePickerMode.day,
+                            firstDate: DateTime(2015),
+                            lastDate: DateTime.now());
+                        if (picked != null)
+                          setState(() {
+                            selectedDate = picked;
+                            _dateController.text =
+                                DateFormat.yMd().format(selectedDate);
+                          });
                       },
                     )),
 
-                // Starting location
                 Padding(
                   padding: EdgeInsets.all(15),
                   child: TextField(
+                    controller: _timeController,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(15)),
-                      labelText: 'location',
-                      hintText: 'location',
+                      prefixIcon: Icon(Icons.access_time),
+                      labelText: 'Time',
+                      hintText: "Time",
                     ),
+                    onTap: () async {
+                      String _hour, _minute, _time;
+                      TimeOfDay selectedTime = TimeOfDay.now();
+                      final TimeOfDay? picked = await showTimePicker(
+                          context: context, initialTime: selectedTime);
+                      if (picked != null)
+                        setState(() {
+                          selectedTime = picked;
+                          _hour = selectedTime.hour.toString();
+                          _minute = selectedTime.minute.toString();
+                          _time = _hour + ' : ' + _minute;
+                          _timeController.text = _time;
+                          _timeController.text = formatDate(
+                              DateTime(2019, 08, 1, selectedTime.hour,
+                                  selectedTime.minute),
+                              [hh, ':', nn, " ", am]).toString();
+                        });
+                    },
                   ),
                 ),
 
@@ -168,7 +213,26 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(30)),
                       ),
-                      onPressed: () {},
+                      onPressed: () {
+                        addFoundData(
+                            _userid,
+                            _dropdownValue,
+                            _brandController.text,
+                            _colorController.text,
+                            _descriptionController.text,
+                            _dateController.text,
+                            _timeController.text);
+                        Fluttertoast.showToast(
+                            msg: "found form is submitted",
+                            toastLength: Toast.LENGTH_SHORT,
+                            gravity: ToastGravity.CENTER,
+                            timeInSecForIosWeb: 1,
+                            backgroundColor: Colors.lightBlueAccent,
+                            textColor: Colors.white,
+                            fontSize: 16.0);
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => HomePage(_userid)));
+                      },
                     )),
               ],
             ))));
